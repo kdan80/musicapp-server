3ee96f3031b74029eae9ac0266dc8b695a02b018
kdan80 <kierandansey80@gmail.com>
2022-07-11 21:15:37 +0100

    Switched to ts-node-dev

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-import queue from './src/routes/queue';
+import queue from '@routes/queue';
 
 const app: Express = express();
 app.use(cors());
--- server.ts
+++ server.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const server = http.createServer(app);
 
 server.listen(process.env.PORT, () => {
-  process.stdout.write(`musicapp-server running on port ${process.env.PORT}...\n`);
+  process.stdout.write(`musicapp-server is running on port ${process.env.PORT}...\n`);
 });
