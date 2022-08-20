858a869da4320e8f1f7cc2d6192d72c437453693
kdan80 <kierandansey80@gmail.com>
2022-08-20 15:09:42 +0100

    Tidied up code

--- src/users/routes/user.login.router.ts
+++ src/users/routes/user.login.router.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response } from 'express'
 import { UserModel }  from '@users'
 import bcrypt from 'bcrypt'
 import config from '../config'
