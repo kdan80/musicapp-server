69ccdbc3f5eccbb643cd0e69267854d90cbe9647
kdan80 <kierandansey80@gmail.com>
2022-08-24 20:21:36 +0100

    Updated upload router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,3 +2,4 @@ export { default as createSong }    from './createSong';
 export { default as error }         from './error';
 export { default as playlist }      from './playlist';  
 export { default as stream }        from './stream';
+export { default as upload }        from './upload';
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,14 +1,15 @@
 import express, { NextFunction, Request, Response } from 'express'
 import multer from 'multer'
 import fs from 'fs'
+import util from 'util'
 
 const router = express.Router()
 
-const temp_storage = process.env.MEDIA;
+const temp_storage = `${process.env.MEDIA}/temp`;
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-    
+        
         cb(null, temp_storage);
     },
     filename: (req, file, cb) => {
@@ -19,18 +20,28 @@ const storage = multer.diskStorage({
         
         // If the user stops the upload we need to remove the partially uploaded file
         req.on("aborted", () => {
-            fs.unlink(`${temp_storage}/${file.originalname}`, () => process.stdout.write("file removed\n"));  
+            fs.unlink(`${temp_storage}/${file.originalname}`, () => console.log("file removed"));  
         });
     }
 });
 
-const upload = multer({storage: storage});
+const upload = multer({storage: storage}).array('album', 30)
 
-router.post('/', ( req: Request, res: Response, next: NextFunction ) => {
+const UploadMiddleware = util.promisify(upload)
+
+router.post('/', async( req: Request, res: Response, next: NextFunction ) => {
 
     try {
 
+        await UploadMiddleware(req, res)
+
+        const info_raw = fs.readFileSync(`${temp_storage}/info.json`, 'utf8')
+        const info = JSON.parse(info_raw)
+
+        return res.status(200).json(info)
     } catch (err) {
         next(err)
     }
 })
+
+export default router
