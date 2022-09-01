0f2ec254177237e871f2afcb7153aab1eb9ece9b
kdan80 <kierandansey80@gmail.com>
2022-09-01 20:55:15 +0100

    Error handling

--- src/audio/middleware/audio.upload_album.middleware.ts
+++ src/audio/middleware/audio.upload_album.middleware.ts
@@ -7,18 +7,19 @@ const storage = multer.diskStorage({
     destination: async(req, file, cb) => {
 
         // Create an upload directory based on the name of the uploaded album title and artist
-        const upload_dir = `${process.env.MEDIA}/${req.body.artist}/${req.body.title}`
+        const upload_dir: string = `${process.env.MEDIA}/${req.body.artist}/${req.body.title}`
 
         try {
 
             // Check if the upload directory exists            
             await fs.promises.access(upload_dir)
     
-            cb(null, upload_dir);
+            // If it does commence the upload
+            return cb(null, upload_dir);
 
         } catch (err: any) {
             
-            // If it does not exist create the upload_dir
+            // If it does not exist create the upload_dir and commence the upload
             if (err.code === 'ENOENT') {
                 await fs.promises.mkdir(upload_dir, { recursive: true })
                 console.log('upload_dir created')
@@ -29,15 +30,20 @@ const storage = multer.diskStorage({
         }
 
     },
-    filename: (req, file, cb) => {
+    filename: async(req, file, cb) => {
 
+        try {
         
-        cb(null, file.originalname);
-        
-        // If the user stops the upload we need to remove the partially uploaded file
-        // req.on('aborted', () => {
-        //     fs.unlinkSync(`${file.path}`);  
-        // });
+            // If the user stops the upload we need to remove the partially uploaded album
+            req.on('aborted', async() => {
+                await fs.promises.rm(`${file.destination}`, { recursive: true });  
+            });
+
+            return cb(null, file.originalname);
+
+        } catch (err) {
+            console.log(err)
+        }
     }
 });
 
@@ -57,11 +63,10 @@ const upload_album = async( req: Request, res: Response, next: NextFunction ) =>
         const info = JSON.parse(info_raw);
         req.body.info = info
         req.body.path = files[0].destination
-
+        
         next()
 
     } catch (err) {
-        console.log('###')
         next(err)
     }
 }
