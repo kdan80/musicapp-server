7ba0aed1a297dd6acf03e41a6c7d8b3c75b4dd63
kdan80 <kierandansey80@gmail.com>
2022-09-03 19:12:26 +0100

    Tidied up code

--- src/audio/models/audio.album.model.ts
+++ src/audio/models/audio.album.model.ts
@@ -1,20 +1,10 @@
-import mongoose, { Schema, Document, PopulatedDoc, ObjectId } from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
 import { SongSchema } from './audio.song.model'
-
-type Track = {
-    number: number,
-    disc_number: number,
-    title: string,
-    path: string,
-    duration: number,
-    genres: [string]
-}
 
 export interface IAlbum {
     title: string,
     artist: string,
     featured_artists: [string],
-    //track_list: [Track],
     track_list: [typeof SongSchema],
     duration: number,
     genres: [string],
@@ -49,12 +39,6 @@ export const AlbumSchema = new Schema<IAlbum>({
         type: String,
         required: false
     }],
-
-    //Track_listing is an array of subdocuments
-    // track_list: [{
-    //     type: Object,
-    //     default: []
-    // }],
 
     track_list: [{
         type: SongSchema,
@@ -111,12 +95,6 @@ export const AlbumSchema = new Schema<IAlbum>({
 
 // Unique together artist/album title
 AlbumSchema.index({ title: 1, artist: 1}, { unique: true })
-
-// Hash the incoming password before we save it
-// AlbumSchema.pre('save', function(next){
-//     this.duration = this.track_listing.length * 1000
-//     next()
-// })
 
 // Compile a model from our schema. This will be used to construct documents and read from documents
 export const AlbumModel = mongoose.model<IAlbum>('Album', AlbumSchema)
