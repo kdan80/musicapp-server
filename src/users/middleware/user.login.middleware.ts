c1c7fafbd9dc85c07e9a893aa38ad1e11a8ae6ab
kdan80 <kierandansey80@gmail.com>
2022-09-12 16:32:23 +0100

    Removed unnecessary middlewares

--- src/users/middleware/user.login.middleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-import config from '../config';
-
-// Middleware for decting if a user is already logged in
-export const superfluous_login = ( req: Request, res: Response, next: NextFunction ) => {
-    if (req.session && req.session.isAuthenticated) return res.status(200).send(config.login.msg_200_superfluous)
-    return next()
-};
