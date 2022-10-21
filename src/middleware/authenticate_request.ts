6ebd53552cff4a228136e9237e7a07f08decdc8a
kdan80 <kierandansey80@gmail.com>
2022-10-21 16:56:47 +0100

    Log remote ip

--- src/middleware/authenticate_request.ts
+++ src/middleware/authenticate_request.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 
 const authenticate_request = ( req: Request, res: Response, next: NextFunction ) => {
 
+    console.log('IP ADRESS: ', req.socket.remoteAddress)
     // Handle cases where the user is already logged in
     if ( req.originalUrl === '/login' && req.session.isAuthenticated ) throw new Error('SUPERFLUOUS_LOGIN')
     if ( req.originalUrl === '/login' ) return next()
