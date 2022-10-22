fbb3d0d0f6ff8930ca0dde2e3cf0d02d602251fb
kdan80 <kierandansey80@gmail.com>
2022-10-22 11:28:03 +0100

    Heroku config

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ const app: Express = express()
 app.use(express.static(__dirname + '/public'))
 
 app.use(cors({
-    origin: true,
+    origin: 'http://192.168.1.21:4000',
     credentials: true,
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
 }))
